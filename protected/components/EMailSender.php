43a090f3373d43936b4e2400310426091c1bf8ab
Mauricio Dominguez <mauricio.dar@gmail.com>
2012-02-26 20:49:11 -0600

    removi comentarios inutiles en EMailSender

--- protected/components/EMailSender.php
+++ protected/components/EMailSender.php
@@ -8,47 +8,11 @@ class EMailSender {
 	
 	public static function sendEmail($body, $subject, $email)
 	{
-
-		/*
-		$connection = Yii::app()->db;
-		$sql = "SELECT email FROM alumno WHERE matricula = ".$matricula;
-		$command = $connection->createCommand($sql);
-		$dataReader = $command->query();
-		*/
 		$message = new YiiMailMessage;
 		$message->setBody($body);
 		$message->setSubject($subject);
 		$message->setTo($email);
 		$message->setFrom(array('dcv-noreply@itesm.mx'=>'DCV'));
 		Yii::app()->mail->send($message);
-		
-		/*
-		
-		$sql = "SELECT email FROM alumno WHERE semestre = -1 AND idcarrera = ".$idcarrera;
-		
-		$command=$connection->createCommand($sql);
-		
-		$dataReader=$command->query();
-	
-		$destinatario = array();
-		
-		$dataReader->bindColumn(1, $mat);
-		
-		while(($row = $dataReader->read())!== false){
-			array_push($destinatario, $mat);
-		}
-		
-		$message = new YiiMailMessage;
-		
-		$message->setBody($body);
-		
-		$message->setSubject($subject);
-		
-		$message->setTo($destinatario);
-
-		$message->setFrom(array('dcv-noreply@itesm.mx'=>'DCV'));
-		
-		Yii::app()->mail->send($message);
-		*/
 	}
 }
